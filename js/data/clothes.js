71c61752c6f40dd2018585ad9baac4123bd568f4
garcia-lopez <mplg070605@gmail.com>
2025-03-31 11:03:26 -0600

    feat: added new functionalily owo

--- js/data/clothes.js
+++ js/data/clothes.js
@@ -1,6 +1,74 @@
-export const clothes = [ 
+// This file contains the data for the clothes options in the game.
+export const imagesArray = [
     {
-        id: 'option1',
-        path: '../assets/images/falda.svg',
+        id: 'first-option',
+        src: '../assets/images/keroppi-clothes/falda.svg',
+    },
+    {
+        id: 'second-option',
+        src: '../assets/images/keroppi-clothes/black-skirt.svg',
+    },
+    {
+        id: 'third-option',
+        src: '../assets/images/keroppi-clothes/stripes-skirt.svg',
+    },
+    {
+        id: 'fourth-option',
+        src: '../assets/images/keroppi-clothes/flower-skirt.svg',
+    },
+    {
+        id: 'fifth-option',
+        src: '../assets/images/keroppi-clothes/spider-skirt.svg',
+    },
+    {
+        id: 'sixth-option',
+        src: '../assets/images/keroppi-clothes/blue-smoking.svg',
+    },
+    {
+        id: 'seventh-option',
+        src: '../assets/images/keroppi-clothes/cat-skirt.svg',
+    },
+    {
+        id: 'eight-option',
+        src: '../assets/images/keroppi-clothes/green-skirt.svg',
+    },
+    {
+        id: 'nineth-option',
+        src: '../assets/images/keroppi-clothes/heart-skirt.svg',
+    },
+    {
+        id: 'tenth-option',
+        src: '../assets/images/keroppi-clothes/black-smoking.svg',
+    },
+    {
+        id: 'eleventh-option',
+        src: '../assets/images/keroppi-clothes/pink-stripes-skirt.svg',
+    },
+    {
+        id: 'twelveth-option',
+        src: '../assets/images/keroppi-clothes/white-green-skirt.svg',
+    },
+];
+
+export const headImagesArray = [ 
+    {
+        id: 'first-option',
+        src: '../assets/images/keroppi-clothes/head-accesories/stars.svg',
+    },
+    {
+        id: 'second-option',
+        src: '../assets/images/keroppi-clothes/head-accesories/flowers.svg',
+    },
+    {
+        id: 'third-option',
+        src: '../assets/images/keroppi-clothes/head-accesories/cat-ears.svg',
+    },
+    {
+        id: 'fourth-option',
+        src: '../assets/images/keroppi-clothes/head-accesories/stars.svg',
+    },
+    {
+        id: 'fifth-option',
+        src: '../assets/images/keroppi-clothes/head-accesories/stars.svg',
     }
 ]
--- js/script.js
+++ js/script.js
@@ -1,31 +1,126 @@
-import { clothes } from "./data/clothes.js";
+import { imagesArray, headImagesArray } from "./data/clothes.js";
 
+const images = document.querySelectorAll('.clothes-image');
+let skirt, head;
 const keroppi_image = document.getElementById('keroppi-image'),
       resetButton = document.getElementById('reset-button'),
-      skirt = document.getElementById('falda-image'),
-      option1 = document.getElementById('first-option'),
-      option2 = document.getElementById('second-option'),
-      option3 = document.getElementById('third-option');
+      elements = [
+        skirt = document.getElementById('falda-image'),
+        head = document.getElementById('head-image'),
+      ],
+      leftArrow = document.getElementById('left-arrow'),
+      rightArrow = document.getElementById('right-arrow');
 
-skirt.style.display = 'none';      
+
+let counter = 0; // start index
+const imagesPerView = 3;
+
+/*Doing the tabs logic */
+let selectedCategory = imagesArray; // Default category
+let selectedOption = skirt; // Default option
+
+const tabMap = {
+  'clothes': [imagesArray, skirt],
+  'head-accesories': [headImagesArray, head],
+  'hand-accesories': 'hand',
+  'keroppi-background': 'background',
+};
+
+function cleanKeroppi() {
+    elements.forEach(el => {
+        el.style.display = 'none';
+      }); // Hide all elements initially
+}
+
+cleanKeroppi(); // Hide all elements initially
+
+Object.keys(tabMap).forEach((tab) => {
+  const tabElement = document.getElementById(tab);
+  tabElement.addEventListener('click', () => {
+    selectedCategory = tabMap[tab][0];
+    counter = 0; // Reset counter when changing tabs
+    updateImages(); // Update images when a new tab is clicked
+    selectedOption = tabMap[tab][1]; // Update the selected option 
+  });
+});
 
 resetButton.addEventListener('click', function() {
     keroppi_image.style.animation = 'none';
-    skirt.style.display = 'none';
+    cleanKeroppi(); // Hide all elements initially
     setTimeout(() => { keroppi_image.style.animation = 'jump-shaking 0.5s'; }, 10); /*To ensure that an animation re-asigns properly, I need to set a small timeout -.-*/
 
     
 });
 
+images.forEach((image) => {
+    image.addEventListener('click', function() {
+        selectedOption.style.display = 'block';
+        selectedOption.src = this.src;
+    });
+})
 
-option1.addEventListener('click', function() {
-    for (let cloth in clothes){
-        if(clothes[cloth].id == 'option1'){
-            skirt.style.display = 'block';
-            skirt.src = clothes[cloth].path;
+
+rightArrow.addEventListener('click', function () {
+    counter += imagesPerView;
+
+    // Si se pasa del final, reinicia al principio
+    if (counter >= selectedCategory.length) {
+        counter = 0;
+    }
+
+    updateImages();
+});
+
+leftArrow.addEventListener('click', function () {
+    counter -= imagesPerView;
+
+    // Si baja de 0, salta al último grupo completo (ajustado para múltiplos de 3)
+    if (counter < 0) {
+        const remainder = selectedCategory.length % imagesPerView;
+        counter = remainder === 0
+            ? selectedCategory.length - imagesPerView
+            : selectedCategory.length - remainder;
+    }
+
+    updateImages();
+});
+
+function updateImages() {
+    for (let i = 0; i < imagesPerView; i++) {
+      const imageData = selectedCategory[counter + i];
+  
+      if (imageData && images[i]) {
+        images[i].src = imageData.src;
+        images[i].style.display = 'block'; // mostrar la imagen si hay datos
+      } else if (images[i]) {
+        images[i].style.display = 'none'; // ocultar si no hay data
+      }
+    }
+  }
+
+
+
+/*
+I can use this function with id in case that I want to add unique css style to an image 
+function updateImages() {
+    for (let i = 0; i < imagesPerView; i++) {
+        const imageData = selectedCategory[counter + i];
+
+        // Asegurarse que no haya overflow
+        if (imageData && images[i]) {
+            images[i].id = imageData.id;
+            images[i].src = imageData.src;
+        } else {
+            // Oculta imagen si no hay suficiente data
+            images[i].style.display = 'none';
         }
     }
 
-});
-
+    // En caso de que hayan estado ocultas antes
+    for (let i = 0; i < imagesPerView; i++) {
+        if (images[i]) {
+            images[i].style.display = 'block';
+        }
+    }
+}*/
 
